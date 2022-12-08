6e630197942623a3f919aa66b40812f22023b96a
Titoy137 <118978184+Titoy137@users.noreply.github.com>
2022-12-08 20:33:46 +0300

    Add files via upload

---
+++ 2 длмашняя/1.pas
@@ -0,0 +1,24 @@
+﻿var
+  a:array[1..5] of integer;
+  i,b,sum,pro,ii,c,m: integer;
+  begin
+    writeln('введите массив из 5 чисел: ');
+    for i := 1 to 5 do begin
+    read(b);
+    a[i] := b;
+    end;
+    for ii := 1 to 5 do begin
+    c:= 1;
+    while c<=5 do begin
+    for c := 1 to 4 do begin
+      sum:= a[ii] + a[ii+c];
+      pro:= a[ii] * a[ii+c];
+      m := ii + c;
+      if sum >= pro then begin
+        writeln(ii,' + ',m ,' >= ',ii,' * ',m );
+        exit
+        end;
+     end;
+     end;
+     end;
+  end.
---
+++ 2 длмашняя/2.pas
@@ -0,0 +1,25 @@
+﻿var
+  a: array[1..20] of integer;
+  b: array[1..20] of integer;
+  i, dd, d,ii, t: integer;
+begin
+  t := 0;
+  writeln('промежуток подстроки: ');
+  read(dd, d);
+  randomize;
+  for i := 1 to 20 do 
+  begin
+    a[i] := random(93) - 20;
+    b[i] := a[i];
+    end;
+    for ii:= 1 to 20 do begin
+    if (ii >= dd) and (ii <= d) then begin
+      b[dd + t] := a[d - t];
+      t += 1;
+    end;
+    if ii >= 20 then begin
+      writeln(a);
+    end;
+  end;
+  write(b);
+end.
